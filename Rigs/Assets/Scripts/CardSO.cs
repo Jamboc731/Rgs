23c7c6a7fef1b1a1e80b0bde5725a871886bb517
Jamie Covell <jamiec.covellj@gmail.com>
2019-01-24 09:45:32 +0000

    did the auto cost. feelin good man

--- Rigs/Assets/Scripts/CardSO.cs
+++ Rigs/Assets/Scripts/CardSO.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[CreateAssetMenu(fileName = "Card", menuName = "New Card")]
 public class CardSO : ScriptableObject
 {
 
+    public new string name;
+    public Texture2D cardImage;
     public int attack;
     public int threshold;
-    public string [] traits;
-    private int cost;
+    public string [] traits = new string[] { };
+    [HideInInspector]
+    public int cost;
     private Traits traitList;
 
 
-    private void Awake ()
+    private void OnEnable ()
     {
-
+        //Debug.Log("Start"); 
         traitList = GameObject.Find ("Manager").GetComponent<Traits>();
 
         cost = FindCost ();
@@ -26,12 +31,13 @@ public class CardSO : ScriptableObject
         int cost = 0;
 
         cost += attack;
-        cost += threshold;
+        cost += threshold - 2;
 
         for (int i = 0; i < traits.Length; i++)
         {
-            int isMultiplicative = System.Array.IndexOf (traitList.multiplicative, traits [i]);
-            int traitNumber = System.Array.IndexOf (traitList.traitName, traits [i]);
+            string temp = traits[i].Substring(0, traits[i].Length - 2);
+            int isMultiplicative = System.Array.IndexOf (traitList.multiplicative, temp);
+            int traitNumber = System.Array.IndexOf (traitList.traitName, temp);
             if (isMultiplicative > -1)
             {
                 cost += traitList.traitCost [traitNumber] * System.Convert.ToInt32(traits[i].Substring(traits[i].Length - 1));
@@ -42,7 +48,7 @@ public class CardSO : ScriptableObject
             }
         }
 
-        cost /= 2;
+        cost = Mathf.FloorToInt(cost/2);
 
         return cost;
     }
