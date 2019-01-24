74964176a0beeae9300a6a84e37dfcacec91447b
JamieCovell <jamiec.covellj@gmail.com>
2019-01-24 07:26:30 +0000

    StartedRigs
    I think I made the cost calc when creating scriptable O's

---
+++ Rigs/Assets/Scripts/CardSO.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSO : ScriptableObject
+{
+
+    public int attack;
+    public int threshold;
+    public string [] traits;
+    private int cost;
+    private Traits traitList;
+
+
+    private void Awake ()
+    {
+
+        traitList = GameObject.Find ("Manager").GetComponent<Traits>();
+
+        cost = FindCost ();
+
+    }
+
+    private int FindCost ()
+    {
+        int cost = 0;
+
+        cost += attack;
+        cost += threshold;
+
+        for (int i = 0; i < traits.Length; i++)
+        {
+            int isMultiplicative = System.Array.IndexOf (traitList.multiplicative, traits [i]);
+            int traitNumber = System.Array.IndexOf (traitList.traitName, traits [i]);
+            if (isMultiplicative > -1)
+            {
+                cost += traitList.traitCost [traitNumber] * System.Convert.ToInt32(traits[i].Substring(traits[i].Length - 1));
+            }
+            else
+            {
+                cost += traitList.traitCost [traitNumber];
+            }
+        }
+
+        cost /= 2;
+
+        return cost;
+    }
+
+}
---
+++ Rigs/Assets/Scripts/Traits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traits : MonoBehaviour
+{
+
+    public string[] traitName = new string [] { };
+    public int[] traitCost = new int [] { };
+    public string [] multiplicative = new string [] { };
+
+}
